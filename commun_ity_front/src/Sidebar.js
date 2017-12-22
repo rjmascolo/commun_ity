96b10413a3dbd865126ccc3c6f2c0292fc1f532b
github username <github email address>
2017-12-22 13:59:28 -0500

    done

--- commun_ity_front/src/Sidebar.js
+++ commun_ity_front/src/Sidebar.js
@@ -10,46 +10,24 @@ export default class SidebarLeft extends Component {
     return (
       <Menu vertical idName="sidebar">
         <Menu.Item>
-          <Menu.Header>Products</Menu.Header>
+          <Menu.Header>Your Dashboard</Menu.Header>
 
           <Menu.Menu>
-            <Menu.Item name='enterprise' active={activeItem === 'enterprise'} onClick={this.handleItemClick} />
-            <Menu.Item name='consumer' active={activeItem === 'consumer'} onClick={this.handleItemClick} />
+            <Menu.Item name='Calendar' active={activeItem === 'enterprise'} onClick={this.handleItemClick} />
+            <Menu.Item name='Events' active={activeItem === 'enterprise'} onClick={this.handleItemClick} />
+            <Menu.Item name='Tasks' active={activeItem === 'consumer'} onClick={this.handleItemClick} />
           </Menu.Menu>
         </Menu.Item>
 
         <Menu.Item>
-          <Menu.Header>CMS Solutions</Menu.Header>
+          <Menu.Header>Browse </Menu.Header>
 
           <Menu.Menu>
-            <Menu.Item name='rails' active={activeItem === 'rails'} onClick={this.handleItemClick} />
-            <Menu.Item name='python' active={activeItem === 'python'} onClick={this.handleItemClick} />
-            <Menu.Item name='php' active={activeItem === 'php'} onClick={this.handleItemClick} />
+            <Menu.Item name='Communities' active={activeItem === 'rails'} onClick={this.handleItemClick} />
+            <Menu.Item name='Events' active={activeItem === 'python'} onClick={this.handleItemClick} />
           </Menu.Menu>
         </Menu.Item>
 
-        <Menu.Item>
-          <Menu.Header>Hosting</Menu.Header>
-
-          <Menu.Menu>
-            <Menu.Item name='shared' active={activeItem === 'shared'} onClick={this.handleItemClick} />
-            <Menu.Item name='dedicated' active={activeItem === 'dedicated'} onClick={this.handleItemClick} />
-          </Menu.Menu>
-        </Menu.Item>
-
-        <Menu.Item>
-          <Menu.Header>Support</Menu.Header>
-
-          <Menu.Menu>
-            <Menu.Item name='email' active={activeItem === 'email'} onClick={this.handleItemClick}>
-              E-mail Support
-            </Menu.Item>
-
-            <Menu.Item name='faq' active={activeItem === 'faq'} onClick={this.handleItemClick}>
-              FAQs
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu.Item>
       </Menu>
     )
   }

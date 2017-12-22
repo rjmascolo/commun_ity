2d12803c762808c3ad8ad8fc0b83690d1a60429c
github username <github email address>
2017-12-22 12:15:45 -0500

    stuff

--- commun_ity_back/app/controllers/users_controller.rb
+++ commun_ity_back/app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     user = User.find(params[:id])
-    render json: user.to_json(only:[:id, :email, :first_name, :last_name, :profile_pic], include: [:tasks, :communities])
+    render json: user.to_json(only:[:id, :email, :first_name, :last_name, :profile_pic], include: [:tasks, :communities, :events])
   end
 
   def create

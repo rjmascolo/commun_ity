d08863d0f178f8e6155d2281005634cb20f9e557
github username <github email address>
2018-01-03 09:56:51 -0500

    added tabs to community browse

--- commun_ity_front/src/container_cards/CommunityBrowseCard.js
+++ commun_ity_front/src/container_cards/CommunityBrowseCard.js
@@ -15,9 +15,10 @@ class CommunityBrowseCard extends React.Component {
   render() {
     const community = this.props.community
     const members = community.members.length
+    console.log(community)
   return (
     <Item>
-      <Item.Image src="http://cdn2us.denofgeek.com/sites/denofgeekus/files/kinopoisk-ru-big-lebowski_2c-the-588419.jpg" />
+      <Item.Image src={community.image_url} />
 
       <Item.Content>
         <Item.Header as='a'>{community.name}</Item.Header>
--- commun_ity_front/src/pages/browse/CommunitiesBrowse.js
+++ commun_ity_front/src/pages/browse/CommunitiesBrowse.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Item} from 'semantic-ui-react'
+import {Item, Tab} from 'semantic-ui-react'
 import CommunityBrowseCard from '../../container_cards/CommunityBrowseCard'
 
 let URL = "http://localhost:3000/communities"
@@ -14,7 +14,6 @@ class CommunitiesBrowse extends React.Component{
   }
 
   render(){
-
     const managedCommunitiesDets = this.state.communities ? this.props.user.managingCommunities.map(community => {
       return (
         <CommunityBrowseCard
@@ -36,7 +35,7 @@ class CommunitiesBrowse extends React.Component{
           />) } )
       : null
 
-
+    // this funtion filters out all of the duplicate data
     const filteredCommunities = this.state.communities ? this.state.communities.filter( community => {
       let userManagingCommunitiesId = this.props.user.managingCommunities.map(community => community.id)
       let userMemberOfId = this.props.user.memberOf.map(community => community.id)
@@ -54,25 +53,18 @@ class CommunitiesBrowse extends React.Component{
           />) } )
       : null
 
+    const panes = [
+      { menuItem: 'Communities You Belong To', render: () => <Tab.Pane><div id="browse-event-card-container"><Item.Group divided>{memberOfCommunitiesDets}</Item.Group></div></Tab.Pane> },
+      { menuItem: 'Search Communities', render: () => <Tab.Pane><div id="browse-event-card-container"><Item.Group divided>{communitiesDets}</Item.Group></div></Tab.Pane> },
+      { menuItem: 'Communities You Manage', render: () => <Tab.Pane><div id="browse-event-card-container"><Item.Group divided>{managedCommunitiesDets}</Item.Group></div></Tab.Pane> },
+    ]
+
     return(
     <div id="user-home-div">
       <h1 id="user-home-welcome">Communities Around You</h1>
       <p id="user-home-welcome">Take a look through the communities in your area</p>
       <br/>
-      <div id="browse-event-card-container">
-        <h1>You manage</h1>
-        <Item.Group divided>
-          {managedCommunitiesDets}
-        </Item.Group>
-        <h1>You Belong To</h1>
-        <Item.Group divided>
-          {memberOfCommunitiesDets}
-        </Item.Group>
-        <h1>Communities You Can Join</h1>
-        <Item.Group divided>
-          {communitiesDets}
-        </Item.Group>
-      </div>
+      <Tab panes={panes} />
     </div>
     )
   }

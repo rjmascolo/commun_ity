3aa15eddf726c44a49646bb014f7d323faf55134
github username <github email address>
2018-01-03 09:41:11 -0500

    something

--- commun_ity_front/src/container_cards/memberCard.js
+++ commun_ity_front/src/container_cards/memberCard.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { List } from 'semantic-ui-react'
+import React from "react";
+import { List } from "semantic-ui-react";
 
-const MemberCard = ({member}) => (
+const MemberCard = ({ member }) => (
   <List.Item>
     <List.Content>
       <List.Header>{member.firstName + " " + member.lastName}</List.Header>
       <List.Description>Joined whenever</List.Description>
     </List.Content>
   </List.Item>
-)
+);
 
-export default MemberCard
+export default MemberCard;

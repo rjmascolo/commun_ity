aa30baaf3cf6639d3e4e44594bcc7f07abe17eb2
github username <github email address>
2018-01-03 10:05:23 -0500

    done

--- commun_ity_front/src/container_cards/CommunityBrowseCard.js
+++ commun_ity_front/src/container_cards/CommunityBrowseCard.js
@@ -12,16 +12,18 @@ class CommunityBrowseCard extends React.Component {
     this.props.community.members.push("")
   }
 
+
   render() {
     const community = this.props.community
     const members = community.members.length
+    const linkURL = `/community/${community.id}`
     console.log(community)
   return (
     <Item>
       <Item.Image src={community.image_url} />
 
       <Item.Content>
-        <Item.Header as='a'>{community.name}</Item.Header>
+        <Item.Header as='a' href= {linkURL} >{community.name}</Item.Header>
         <Item.Meta>
           <span className='cinema'>{community.location}</span>
         </Item.Meta>
